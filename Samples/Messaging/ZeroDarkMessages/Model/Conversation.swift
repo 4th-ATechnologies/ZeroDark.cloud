31f68aaced7112ba36a25f25fe1ede5952705ea3
Vinnie Moscaritolo <vinnie@4th-a.com>
2019-06-04 17:20:19 -0700

    fix compile for message demo

--- Samples/Messaging/ZeroDarkMessages/Model/Conversation.swift
+++ Samples/Messaging/ZeroDarkMessages/Model/Conversation.swift
@@ -10,51 +10,29 @@ import ZeroDarkCloud
 
 let kZDMCollection_Conversation = "ZDM_Conversation"
 
-class Conversation: NSObject, NSCopying, Codable, CloudEncodable  {
+class Conversation: NSObject, NSCopying, Codable  {
 	
 	enum CodingKeys: String, CodingKey {
 		case uuid = "uuid"
 		case localUserID = "localUserID"
+		case remoteUserID = "remoteUserID"
 	}
 	
 	let uuid: String
 	let localUserID: String
- 	var title: String
+	let remoteUserID: String
 	
-	init(uuid: String, localUserID: String) {
+	init(uuid: String, localUserID: String, remoteUserID: String) {
 		self.uuid = uuid
 		self.localUserID = localUserID
+		self.remoteUserID = remoteUserID
 	}
 	
-	convenience init(localUserID: String) {
+	convenience init(localUserID: String, remoteUserID: String) {
 		let _uuid = UUID().uuidString
-		self.init(uuid: _uuid, localUserID: localUserID)
+		self.init(uuid: _uuid, localUserID: localUserID, remoteUserID: remoteUserID)
 	}
-	
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// MARK: CloudCodable
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	init(fromCloudData cloudData: Data, node: ZDCNode) throws {
-		
-		let decoder = JSONDecoder()
-		let cloudJSON = try decoder.decode(ConversationCloudJSON.self, from: cloudData)
-		
-		self.uuid = UUID().uuidString
-		self.localUserID = node.localUserID
-		
-		self.title = cloudJSON.title
-	}
-	
-	func cloudEncode() throws -> Data {
-		
-		let cloudJSON = ConversationCloudJSON(fromConversation: self)
-		
-		let encoder = JSONEncoder()
-		let data = try encoder.encode(cloudJSON)
-		
-		return data
-	}
+
 	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// MARK: NSCopying
@@ -63,27 +41,18 @@ class Conversation: NSObject, NSCopying, Codable, CloudEncodable  {
 	func copy(with zone: NSZone? = nil) -> Any {
 		
 		let copy = Conversation(uuid        : uuid,
-							 	localUserID : localUserID)
+							 	localUserID : localUserID, remoteUserID: remoteUserID)
 		return copy
 	}
+	
+//	func cloudData() -> Data {
+//		
+//		let json = {
+//			"userID": self.remoteUserID
+//		}
+//		let data = JSONEncoder.encode(json)
+//		return data
+//	}
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// MARK: -
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// We only store a subset of the object in the cloud.
-/// This class acts as a JSON wrapper for the information that gets encoded/decoded into JSON for cloud storage.
-///
-class ConversationCloudJSON: Codable {
-	
-	enum CodingKeys: String, CodingKey {
- 	case title = "title"
-	}
-	
- 	var title: String
-	
-	init(fromConversation conversation: Conversation) {
-// 	self.title = list.title
-	}
-}
