2d237dc83344c97a4e5fdb0b0c55c294c6c47e84
Robbie Hanson <robbiehanson@deusty.com>
2019-10-15 17:20:05 -0700

    Updating URLs

--- Samples/ZeroDarkTodo/ZeroDarkTodo/ZDCManager.swift
+++ Samples/ZeroDarkTodo/ZeroDarkTodo/ZDCManager.swift
@@ -2,8 +2,8 @@
 ///
 /// Homepage      : https://www.zerodark.cloud
 /// GitHub        : https://github.com/4th-ATechnologies/ZeroDark.cloud
-/// Documentation : https://zerodarkcloud.readthedocs.io/en/latest/
-/// API Reference : https://4th-atechnologies.github.io/ZeroDark.cloud/
+/// Documentation : https://zerodarkcloud.readthedocs.io/en/latest
+/// API Reference : https://apis.zerodark.cloud
 ///
 /// Sample App: ZeroDarkTodo
 
