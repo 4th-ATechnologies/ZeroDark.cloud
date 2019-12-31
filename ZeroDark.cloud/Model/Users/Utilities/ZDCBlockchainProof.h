d740282379e7a94b59d6de950ce838b82d16acc9
Robbie Hanson <robbiehanson@deusty.com>
2019-12-30 16:37:07 -0800

    Adding missing documentation

--- ZeroDark.cloud/Model/Users/Utilities/ZDCBlockchainProof.h
+++ ZeroDark.cloud/Model/Users/Utilities/ZDCBlockchainProof.h
@@ -17,11 +17,39 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface ZDCBlockchainProof : NSObject <NSSecureCoding, NSCopying>
 
+/**
+ * The merkleTreeRoot value is stored on the blockchain (i.e. within the smart contract).
+ *
+ * This value is a reference to a merkleTreeFile that can be fetched from the server.
+ * For example, if the merkleTreeRoot value is
+ * "cd59b7bda6dc1dd82cb173d0cdfa408db30e9a747d4366eb5b60597899eb69c1",
+ * then you could fetch the corresponding JSON file at
+ * https://blockchain.storm4.cloud/cd59b7bda6dc1dd82cb173d0cdfa408db30e9a747d4366eb5b60597899eb69c1.json
+ *
+ * The RestManager has an API to fetch this file for you, given a merkleTreeRoot value:
+ * `-[ZDCRestManager fetchMerkleTreeFile:completionQueue:completionBlock:]`
+ *
+ * The merkleTreeFile is a JSON file that allows you to independently verify the public key information.
+ * You can use the ZDCMerkleTree class to parse & validate this file.
+ */
 @property (nonatomic, readonly) NSString *merkleTreeRoot;
 
+/**
+ * The smart contract is coded such that, when a user's merkleTreeRoot is stored in the contract,
+ * it also records the blockNumber at which the proof was submitted.
+ *
+ * This information can be used to determine when the user's publicKey proof was posted to the blockchain.
+ */
 @property (nonatomic, readonly) NSUInteger blockNumber;
 
+/**
+ * Stores the pubKey value that was recorded in the merkleTreeFile.
+ */
 @property (nonatomic, readonly) NSString *merkleTreeFile_pubKey;
+
+/**
+ * Stores the keyID value that was recorded in the merkleTreeFile.
+ */
 @property (nonatomic, readonly) NSString *merkleTreeFile_keyID;
 
 @end
